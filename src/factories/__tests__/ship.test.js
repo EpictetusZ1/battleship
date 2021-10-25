23446c6225fd26e27c76030cbb19c612a17a96ac
J Heaton <jackrobertheaton@gmail.com>
2021-10-25 18:21:38 -0400

    Add test for sunk ship

--- src/factories/__tests__/ship.test.js
+++ src/factories/__tests__/ship.test.js
@@ -1,18 +1,17 @@
 import {Ship} from "../ship";
 
-let myShip = new Ship(5, [1, 2])
-
-const mockHit = jest.fn( () => {
-    return [1, 3]
-})
-
 describe("Evaluate Ship Public Methods ", () => {
+    let myShip = new Ship(5, [1, 2])
+
+    const mockHit = jest.fn( () => [1, 2])
 
     test("Check object output", () => {
+        mockHit.mockReturnValue([1, 3])
         expect(myShip).toMatchObject({
             len: [0, 0, 0, 0, 0],
             position: [1, 2],
             axis: "x",
+            sunk: false,
             hitArea: [
                 [1, 2],
                 [1, 3],
@@ -23,8 +22,30 @@ describe("Evaluate Ship Public Methods ", () => {
         })
     })
 
-    test.skip("Check if Ship updates its value(s) after hit()", () => {
-        expect(myShip.hit(mockHit)).toEqual(myShip.hitArea[0] === 1)
+    test("Check if Ship updates its value(s) after hit()", () => {
+        expect(myShip.hit(mockHit)).toEqual(myShip.len[0] === 1)
+    })
+
+    test("Check if ship is sunk after N of hits", () => {
+        myShip.hit([1, 2])
+        myShip.hit([1, 3])
+        myShip.hit([1, 4])
+        myShip.hit([1, 5])
+        myShip.hit([1, 6])
+
+        expect(myShip).toMatchObject({
+            len: [1, 1, 1, 1, 1],
+            position: [1, 2],
+            axis: "x",
+            sunk: true,
+            hitArea: [
+                [1, 2],
+                [1, 3],
+                [1, 4],
+                [1, 5],
+                [1, 6],
+            ]
+        })
     })
 
 })
