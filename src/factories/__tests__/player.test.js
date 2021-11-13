0c3a7befc8d4b8c76855e68d2f753de21ed81732
J Heaton <jackrobertheaton@gmail.com>
2021-11-13 10:23:29 -0500

    Add tests for Player obj.

---
+++ src/factories/__tests__/player.test.js
@@ -0,0 +1,11 @@
+import {Player} from "../player";
+
+describe("Evaluate Player Object", () => {
+
+    test("Check Players ability to Attack Game board.", () => {
+
+    })
+
+    test("Check players turn is changed after attack.")
+
+})
