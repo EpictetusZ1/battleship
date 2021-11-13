4f20695b86de070e88014099d1c87fb424ed4d33
J Heaton <jackrobertheaton@gmail.com>
2021-11-13 11:34:08 -0500

    Add logic to computer moves, and error handle duplicate hits

--- src/factories/__tests__/player.test.js
+++ src/factories/__tests__/player.test.js
@@ -4,13 +4,22 @@ describe("Evaluate Player Object", () => {
     const myPlayer = new Player("Jack")
     const mockHit = [5, 5]
 
+    const myComputer = new Player("Bob the robot", true)
+
     test("Check Players ability to Attack Game board.", () => {
         expect(myPlayer.attack(mockHit)).toStrictEqual([5, 5])
     })
 
-    test("Check players turn is changed after attack.", () => {
-        myPlayer.attack(mockHit)
-        expect(myPlayer.isTurn).toBe(false)
+    test("Check computer can generate attack coords array", () => {
+        expect(Array.isArray(myComputer.computerMove())).toBe(true)
     })
 
+    let coords = myComputer.computerMove()
+    test("Check coords are 0 - 9", () => {
+        expect(coords[0] <= 9 && coords[0] >= 0).toBe(true)
+    })
+
+    test("Check coords are 0 - 9", () => {
+        expect(coords[1] <= 9 && coords[0] >= 0).toBe(true)
+    })
 })
--- src/factories/player.js
+++ src/factories/player.js
@@ -3,18 +3,32 @@ export class Player {
     name = "Player 1"
     constructor(name, isComputer = false) {
         this.name = name
-        this.isTurn = true
         this.isComputer = isComputer
+        this.alreadyHit = []
+    }
+
+    alertBadCoord(message) {
+        return new Error(message)
+    }
+
+    checkIsAlreadyHit(coords) {
+        if (this.alreadyHit.includes(coords)) {
+            if (this.isComputer === false) this.alertBadCoord("This coordinate has already been hit")
+            else this.computerMove()
+        }
     }
 
     attack(e) {
-        if (this.isTurn) {
-            this.isTurn = false
-            return [e[0], e[1]] // Coords to send -> board based off e.target.data
-        }
-
+        this.alreadyHit.push( [e[0], e[1]] )
+        return [e[0], e[1]] // Coords to send -> board based off e.target.data
     }
 
+    computerMove() {
+        let x = Math.floor((Math.random() * 9) + 1)
+        let y = Math.floor((Math.random() * 9) + 1)
+        console.log([x, y])
 
-
+        this.checkIsAlreadyHit([x, y])
+        return this.attack([x, y])
+    }
 }
