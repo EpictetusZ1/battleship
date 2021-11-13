27604c960b9512a719fa3fbb1054ae2e438ca994
J Heaton <jackrobertheaton@gmail.com>
2021-11-13 10:39:34 -0500

    Create basic player

--- src/factories/__tests__/player.test.js
+++ src/factories/__tests__/player.test.js
@@ -1,11 +1,16 @@
 import {Player} from "../player";
 
 describe("Evaluate Player Object", () => {
+    const myPlayer = new Player("Jack")
+    const mockHit = [5, 5]
 
     test("Check Players ability to Attack Game board.", () => {
-
+        expect(myPlayer.attack(mockHit)).toStrictEqual([5, 5])
     })
 
-    test("Check players turn is changed after attack.")
+    test("Check players turn is changed after attack.", () => {
+        myPlayer.attack(mockHit)
+        expect(myPlayer.isTurn).toBe(false)
+    })
 
 })
--- src/factories/player.js
+++ src/factories/player.js
@@ -1,3 +1,20 @@
+
 export class Player {
+    name = "Player 1"
+    constructor(name, isComputer = false) {
+        this.name = name
+        this.isTurn = true
+        this.isComputer = isComputer
+    }
+
+    attack(e) {
+        if (this.isTurn) {
+            this.isTurn = false
+            return [e[0], e[1]] // Coords to send -> board based off e.target.data
+        }
+
+    }
+
+
 
 }
